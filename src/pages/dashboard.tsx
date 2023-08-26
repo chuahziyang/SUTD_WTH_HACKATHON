d373c6fa97bac14747a32c44100813da35a188f3
Victor <victor.j.zhao@gmail.com>
2023-08-27 01:05:38 +0800

    fix bugs

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,8 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-import { ArrowSmDownIcon, ArrowSmUpIcon } from '@heroicons/react/solid'
+import { Fragment, useEffect } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { ArrowSmDownIcon, ArrowSmUpIcon } from "@heroicons/react/solid";
 import Wrapper from "@components/components/wrapper";
 import supabase from "../utils/supabaseClient";
 import { useRouter } from "next/router";
@@ -15,8 +15,18 @@ const stats = [
 ];
 
 const activites = [
-  { date: '27th Aug 2023', time: '04:00 PM', activity: 'Walked a total distance of 5.5km', pts: '600' },
-  { date: '26th Aug 2023', time: '08:59 AM', activity: 'Carpooled a total distance of 3.4km', pts: '475'}
+  {
+    date: "27th Aug 2023",
+    time: "04:00 PM",
+    activity: "Walked a total distance of 5.5km",
+    pts: "600",
+  },
+  {
+    date: "26th Aug 2023",
+    time: "08:59 AM",
+    activity: "Carpooled a total distance of 3.4km",
+    pts: "475",
+  },
   // More people...
 ];
 
@@ -80,17 +90,21 @@ export default function Dashboard() {
             </div>
           </header>
           <main>
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
- <div>
-      <h1 className="text-4xl leading-10 font-bold text-gray-900">Dashboard</h1>
-      <dl className="mt-5 grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x">
-        {stats.map((item) => (
-          <div key={item.name} className="px-4 py-5 sm:p-6">
-            <dt className="text-base font-medium text-center text-gray-900">{item.name}</dt>
-            <dd className="mt-1 flex justify-center items-baseline md:block lg:flex">
-              <div className="text-7xl py-8 font-semibold text-[#009278]">
-                {item.stat}
-              </div>
+            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+              <div>
+                <h1 className="text-4xl font-bold leading-10 text-gray-900">
+                  Dashboard
+                </h1>
+                <dl className="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-x md:divide-y-0">
+                  {stats.map((item) => (
+                    <div key={item.name} className="px-4 py-5 sm:p-6">
+                      <dt className="text-center text-base font-medium text-gray-900">
+                        {item.name}
+                      </dt>
+                      <dd className="mt-1 flex items-baseline justify-center md:block lg:flex">
+                        <div className="py-8 text-7xl font-semibold text-[#009278]">
+                          {item.stat}
+                        </div>
 
                         {/* <div
                 className={classNames(
@@ -113,74 +127,93 @@ export default function Dashboard() {
                 <span className="sr-only">{item.changeType === 'increase' ? 'Increased' : 'Decreased'} by</span>
                 {item.change}
               </div> */}
-            </dd>
-          </div>
-        ))}
-      </dl>
-    </div>
-    <div className="px-4 sm:px-6 lg:px-8 mt-10">
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Users</h1>
-          <p className="mt-2 text-sm text-gray-700">
-            A list of all activities within the past 30 days including their date, time, and a point rewards summary.
-          </p>
-        </div>
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-          <button
-            type="button"
-            className="inline-flex items-center justify-center rounded-md border border-transparent bg-[#009278] px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-[#009278] focus:outline-none focus:ring-2 focus:ring-[#009278] focus:ring-offset-2 sm:w-auto"
-          >
-            New Activity
-          </button>
-        </div>
-      </div>
-      <div className="mt-8 flex flex-col">
-        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-gray-50">
-                  <tr className="divide-x divide-gray-200">
-                    <th scope="col" className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                      Date/Time
-                    </th>
-                    <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      Activity
-                    </th>
-                    <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      Points Awarded
-                    </th>
-                    {/* <th scope="col" className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6">
+                      </dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+              <div className="mt-10 px-4 sm:px-6 lg:px-8">
+                <div className="sm:flex sm:items-center">
+                  <div className="sm:flex-auto">
+                    <h1 className="text-xl font-semibold text-gray-900">
+                      Users
+                    </h1>
+                    <p className="mt-2 text-sm text-gray-700">
+                      A list of all activities within the past 30 days including
+                      their date, time, and a point rewards summary.
+                    </p>
+                  </div>
+                  <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    <button
+                      type="button"
+                      className="inline-flex items-center justify-center rounded-md border border-transparent bg-[#009278] px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-[#009278] focus:outline-none focus:ring-2 focus:ring-[#009278] focus:ring-offset-2 sm:w-auto"
+                    >
+                      New Activity
+                    </button>
+                  </div>
+                </div>
+                <div className="mt-8 flex flex-col">
+                  <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                      <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                        <table className="min-w-full divide-y divide-gray-300">
+                          <thead className="bg-gray-50">
+                            <tr className="divide-x divide-gray-200">
+                              <th
+                                scope="col"
+                                className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                              >
+                                Date/Time
+                              </th>
+                              <th
+                                scope="col"
+                                className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
+                              >
+                                Activity
+                              </th>
+                              <th
+                                scope="col"
+                                className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
+                              >
+                                Points Awarded
+                              </th>
+                              {/* <th scope="col" className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6">
                       Role
                     </th> */}
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white">
-                  {activites.map((activity) => (
-                    <tr key={activity.date} className="divide-x divide-gray-200">
-                      <td className="whitespace-nowrap py-4 pl-4 text-sm font-medium text-gray-900 sm:pl-6">
-                        {activity.date}<span className="whitespace-nowrap pb-1 pl-2 align-middle text-xs text-gray-500">at {activity.time}</span> 
-                      </td>
-                      <td className="whitespace-nowrap p-4 text-sm text-gray-500">{activity.activity}</td>
-                      <td className="whitespace-nowrap p-4 text-sm text-gray-500">{activity.pts}</td>
-                      {/* <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6">{person.role}</td> */}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                            </tr>
+                          </thead>
+                          <tbody className="divide-y divide-gray-200 bg-white">
+                            {activites.map((activity) => (
+                              <tr
+                                key={activity.date}
+                                className="divide-x divide-gray-200"
+                              >
+                                <td className="whitespace-nowrap py-4 pl-4 text-sm font-medium text-gray-900 sm:pl-6">
+                                  {activity.date}
+                                  <span className="whitespace-nowrap pb-1 pl-2 align-middle text-xs text-gray-500">
+                                    at {activity.time}
+                                  </span>
+                                </td>
+                                <td className="whitespace-nowrap p-4 text-sm text-gray-500">
+                                  {activity.activity}
+                                </td>
+                                <td className="whitespace-nowrap p-4 text-sm text-gray-500">
+                                  {activity.pts}
+                                </td>
+                                {/* <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6">{person.role}</td> */}
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </main>
         </div>
       </Wrapper>
-      </Wrapper>
     </>
   );
-  );
 }
-
