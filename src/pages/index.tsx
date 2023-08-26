503ca7aec9d716228b5f7b538e37b77252f7e02e
keith <keithonghx@gmail.com>
2023-08-26 14:30:56 +0800

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "@components/utils/api";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
