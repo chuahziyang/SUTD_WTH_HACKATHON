7dd3fea9db75cec7163c348fce28bcc1441cb131
czyan <czyang2002@gmail,com>
2023-08-26 14:45:14 +0800

    gxfdszf

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      thegreen: "#009278",
+      thelightgreen: "#cdf4ec",
+      theorange: "#ff8100",
+    },
   },
   plugins: [],
 } satisfies Config;
