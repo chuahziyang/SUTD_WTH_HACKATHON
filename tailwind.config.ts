93c0f59f0d8ba029e9583c983958b149a6f10a82
Victor <victor.j.zhao@gmail.com>
2023-08-26 16:28:36 +0800

    draft signup and login pages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-green": "#009278",
+        "custom-green-hover": "#007f66",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
