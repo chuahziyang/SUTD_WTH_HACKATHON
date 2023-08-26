b05bbc08d5e70c4545034ca409465bef2a958591
Victor <victor.j.zhao@gmail.com>
2023-08-27 03:05:14 +0800

    remove activity button

--- src/components/activity.tsx
+++ src/components/activity.tsx
@@ -34,20 +34,11 @@ export default function Activity() {
     <div className="mt-10 px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Users</h1>
+          <h1 className="text-xl font-semibold text-gray-900">Activity</h1>
           <p className="mt-2 text-sm text-gray-700">
             A list of all activities within the past 30 days including their
             date, time, and a point rewards summary.
           </p>
-        </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <button
-            type="button"
-            className="inline-flex items-center justify-center rounded-md border border-transparent bg-[#009278] px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-[#009278] focus:outline-none focus:ring-2 focus:ring-[#009278] focus:ring-offset-2 sm:w-auto"
-            onClick={handleNewActivity}
-          >
-            New Activity
-          </button>
         </div>
       </div>
       <div className="mt-8 flex flex-col">
